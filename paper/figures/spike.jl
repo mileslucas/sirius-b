94b5b0d23b8fdf275e925f86a7ec0137f3164fd8
Miles Lucas <mdlucas@hawaii.edu>
2021-11-16 15:49:11 -1000

    update spike figure

--- paper/figures/spike.jl
+++ paper/figures/spike.jl
@@ -10,12 +10,12 @@ pro.rc["image.origin"] = "lower"
 pro.rc["image.cmap"] = "inferno"
 pro.rc["grid"] = false
 
-data_cube = fits.getdata(datadir("epoch_2020feb04", "processed", "2020feb04_sirius-b_cube_calib.fits"))
-frame = data_cube[88, :, :]
+data_cube = fits.getdata(datadir("epoch_2020nov21", "processed", "2020nov21_sirius-b_cube_calib.fits"))
+frame = Float32.(data_cube[169, :, :])
 
-fig, axs = pro.subplots(figwidth="3.5in")
+fig, axs = pro.subplots(figwidth="4in")
 ax = first(axs)
 vmin, vmax = zscale(frame)
 ax.imshow(frame; vmin, vmax, colorbar="r")
-ax.format(xlabel="x", ylabel="y")
+ax.format(xlabel="x [px]", ylabel="y [px]")
 fig.save(figuredir("spike.pdf"))
